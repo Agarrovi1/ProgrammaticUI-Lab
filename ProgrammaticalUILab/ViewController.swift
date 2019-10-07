85cd57fcc94745eeb3c18f824d89b2f18369ec38
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-07 14:07:38 -0400

    initial commit

---
+++ ProgrammaticalUILab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammaticalUILab
+//
+//  Created by Angela Garrovillas on 10/7/19.
+//  Copyright © 2019 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
