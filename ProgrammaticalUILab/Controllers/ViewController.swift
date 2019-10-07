a3cd3d9214d99659f36416d8b826b56437eb2476
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-10-07 16:12:33 -0400

    makes buttons, labels and a imageView and adds to subView

--- ProgrammaticalUILab/Controllers/ViewController.swift
+++ ProgrammaticalUILab/Controllers/ViewController.swift
@@ -9,6 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: - Properties
+    var color = Color.generateRandomRGBValues()
+    var score = ScoreBoard()
+    
+    
+    //MARK: - Buttons
+    lazy var redButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Red", for: .normal)
+        button.titleLabel?.textColor = .red
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
+    lazy var greenButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Green", for: .normal)
+        button.titleLabel?.textColor = .green
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
+    lazy var blueButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Blue", for: .normal)
+        button.titleLabel?.textColor = .blue
+        button.backgroundColor = .blue
+        button.addTarget(self, action: #selector(buttonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Labels
+    lazy var scoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Score:"
+        return label
+    }()
+    lazy var highScoreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "High Score:"
+        return label
+    }()
+    lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Guess the closest color!"
+        return label
+    }()
+    
+    //MARK: - ImageView
+    lazy var colorView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = self.color.changeToUIColor()
+        return imageView
+    }()
+    
+    //MARK: - Functions
+    private func setupSubViews() {
+        self.view.addSubview(redButton)
+        redButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(greenButton)
+        greenButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(blueButton)
+        blueButton.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(scoreLabel)
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(highScoreLabel)
+        highScoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(infoLabel)
+        infoLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(colorView)
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
